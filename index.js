dec01cad587290a726e7cb4b0758b54b4f81b8e8
felixjiayeahnet <felixjia@yeah.net>
2019-11-09 15:45:32 +0800

    组建基础模板

--- index.js
+++ index.js
@@ -1,30 +1,114 @@
-const axios = require('axios')
-
+const axios = require("axios")
+const _ = require("lodash")
+const bodyParser = require("body-parser")
 const express = require("express");
 const app = express();
 const port = 6777;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+
+
+const protocolMesTemplate = {
+    title: "",
+    text: ""
+}
+
+const destructTentMes = (body) => {
+    const template = _.cloneDeep(protocolMesTemplate);
+    /**
+     * 获取 text
+     * @param {*} text 
+     */
+    const getMarkdownTextTemp = (text) => {
+        return `> ${text}`
+    }
+    const getUsername = (pusher) => {
+        const {
+            name,
+            username,
+        } = pusher;
+        return name ? name : username;
+    }
+
+    const getEntranceTitle = (resultTexts, body) => {
+        const {
+            pusher
+        } = body;
+        const pusherName = getUsername(pusher);
+        resultTexts = resultTexts + `${pusherName}\n`;
+        return resultTexts;
+    }
+
+    const getCardTitle = (resultTexts, body) => {
+
+        const {
+            pusher,
+            repository: {
+                name: repoName
+            },
+            ref
+        } = body;
+        const pusherName = getUsername(pusher);
+        const cardTitle = `${pusherName} to ${ref} at ${repoName}\n`;
+        resultTexts = resultTexts + cardTitle;
+        return resultTexts;
+    }
+
+    const getCardBody = (resultTexts, body) => {
+        const {
+            commits
+        } = body;
+        commits.forEach(commit => {
+            const {
+                message
+            } = commit;
+
+            const resultMes = getMarkdownTextTemp(message);
+            resultTexts = resultTexts + resultMes;
+        });
+        return resultTexts;
+    }
+    // 聊天窗口的 title
+    let entranceTitle = "";
+    template.title = getEntranceTitle(entranceTitle, body);
+    // 具体内容的 title 的具体 commits
+    let resultTexts = "";
+    resultTexts =  getCardTitle(resultTexts, body);
+    resultTexts = getCardBody(resultTexts, body);
+    template.text = resultTexts;
+    return template;
+}
+/**
+ * 获取钉钉的内容
+ */
+const getDingMes = (protocolMesTemplate) => {
+
+    const dingdingReq = {
+        msgtype: "markdown",
+        markdown: protocolMesTemplate,
+        isAtAll: false
+    }
+    return dingdingReq;
+}
 
 app.post("*", async (req, res) => {
 
     const {
         originalUrl,
-        query: {
-            access_token
-        },
         body
     } = req;
 
-    const connnetMarkdown = {
-        title: "test title",
-        text: "这是一个 test 提交"
-    }
+    console.log("body" + body);
+    const preTemp = destructTentMes(body);
 
+    // 拼装请求
+    const dingdingReq = getDingMes(preTemp);
 
-    const dingdingReq = {
-        msgtype: "markdown",
-        markdown: connnetMarkdown,
-        isAtAll: false
-    }
+    // 发送请求
     const dingUrl = `https://oapi.dingtalk.com${originalUrl}`;
 
     const dingResult = await axios.post(dingUrl, dingdingReq);
@@ -37,4 +121,4 @@ app.post("*", async (req, res) => {
 
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`tending bot listening on port ${port}!`))
