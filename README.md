e1a36d2498a8faa008ce9b0483db947683b1225f
felixjiayeahnet <felixjia@yeah.net>
2019-11-09 13:59:23 +0800

    可以转发固定在代码中的内容请求

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+## 腾讯的 webhook 定义
+https://cloud.tencent.com/document/product/857/18028
+
+
+## 钉钉的 webhook 定义
+
+https://ding-doc.dingtalk.com/doc#/serverapi2/qf2nxq
+
+https://juejin.im/post/5d5e67c86fb9a06b2766d304
--- index.js
+++ index.js
@@ -1,7 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 6777
+const axios = require('axios')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require("express");
+const app = express();
+const port = 6777;
+
+app.post("*", async (req, res) => {
+
+    const {
+        originalUrl,
+        query: {
+            access_token
+        },
+        body
+    } = req;
+
+    const connnetMarkdown = {
+        title: "test title",
+        text: "这是一个 test 提交"
+    }
+
+
+    const dingdingReq = {
+        msgtype: "markdown",
+        markdown: connnetMarkdown,
+        isAtAll: false
+    }
+    const dingUrl = `https://oapi.dingtalk.com${originalUrl}`;
+
+    const dingResult = await axios.post(dingUrl, dingdingReq);
+
+    const {
+        data
+    } = dingResult;
+
+    res.send(data);
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
